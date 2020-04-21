e4f8fffbdbadc1b44d5be9190aafa017cf7ae758
Albin <96afo02@gmail.com>
2020-04-21 14:00:51 +0200

    Kommenterade koden inget annat

--- src/Actor.gd
+++ src/Actor.gd
@@ -4,6 +4,5 @@ class_name Actor
 const FLOOR_NORMAL: = Vector2.UP
 
 export var gravity: = 4000.0 
-export var speed: = Vector2(300.0, 1000.0)
-var velocity: = Vector2.ZERO
-
+export var speed: = Vector2(300.0, 1000.0) # how quickly the actor is allowed to move
+var velocity: = Vector2.ZERO # how quickly the actor moves
--- src/Player.gd
+++ src/Player.gd
@@ -5,6 +5,7 @@ func _physics_process(delta):
 	velocity = calculate_move_velocity(velocity, direction, speed)
 	velocity = move_and_slide(velocity, FLOOR_NORMAL)
 
+# Calculates the movement/fall speed
 func calculate_move_velocity(
 		linear_velocity: Vector2,
 		direction: Vector2, 
@@ -13,11 +14,14 @@ func calculate_move_velocity(
 	var new_velocity: = linear_velocity
 	new_velocity.x = speed.x * direction.x
 	new_velocity.y += gravity * get_physics_process_delta_time()
+	# If jumping overrule y axis velocity
 	if direction.y == -1.0:
 		new_velocity.y = speed.y * direction.y
 	return new_velocity
 
-
+# Returns the direction of user input for the player movement
+# Key bindings for move_right, move_left and jump can be changed 
+# in Project -> Project settings -> Input Map
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
