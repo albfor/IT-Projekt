daeac2ec289b489cd0fd0271818f244db225cce4
Albin <96afo02@gmail.com>
2020-04-22 15:42:13 +0200

    Exempel på hur interaction med pc kan se ut.

--- src/Player.gd
+++ src/Player.gd
@@ -27,3 +27,13 @@ func get_direction() -> Vector2:
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
 	)
+
+
+
+# Show label saying "press e to enter" when on a PC
+func _on_Area2D_area_entered(area):
+	print(area.get_name())
+	$Label.show()
+
+func _on_Area2D_area_exited(area):
+	$Label.hide()
